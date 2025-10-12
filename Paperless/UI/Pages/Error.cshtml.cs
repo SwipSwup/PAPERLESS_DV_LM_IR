5e7080c4c0c93858d925e6033ccab86924e47254
Ivo <radicivo255@gmail.com>
2025-10-12 13:29:07 +0200

    finished frontend + bug fixes + quality of life (WIP)

---
+++ Paperless/UI/Pages/Error.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+namespace UI.Pages
+{
+    public class ErrorModel : PageModel
+    {
+        public string? RequestId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public void OnGet()
+        {
+            RequestId = System.Diagnostics.Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+        }
+    }
+}
---
+++ Paperless/UI/Pages/Tags/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UI.Pages.Tags
+{
+    public class TagsIndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
